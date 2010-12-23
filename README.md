3f96859756f8428c1b6a20cd26c9e08d539606f2
Stefan Bühler <stbuehler@web.de>
2010-12-23 16:18:34 +0100

    fix example/readme

--- README.md
+++ README.md
@@ -8,29 +8,7 @@
 
 ## usage
 
-	var DHT = require('dht').DHT;
-	var dht = new DHT(51414);
-
-	// require('dht').debug = true;
-
-	var dht = new DHT();
-	// for bootstrapping you need to know a node which already is in the dht
-	dht.bootstrap([ { 'address': 'xxx.xxx.xxx.xxx', 'port': xxx } ]);
-
-	function hex2buf(s) {
-		var buf = new Buffer(s.length >> 1), pos, x;
-		for (pos = 0; pos < buf.length; ++pos) {
-			buf[pos] = parseInt(s.slice(2*pos, 2*pos+2), 16);
-		}
-		return buf;
-	}
-
-	var id = hex2buf("640FE84C613C17F663551D218689A64E8AEBEABE");
-
-	dht.lookup(id, function (peers, finished) {
-		console.log("Found more peers: %j", peers);
-		if (finished) console.log("Lookup done");
-	});
+See example.js
 
 ## status
 
--- example.js
+++ example.js
@@ -1,28 +1,15 @@
-# dht
+var DHT = require('dht');
+var dht = new DHT.DHT(51414);
 
-[DHT](http://bittorrent.org/beps/bep_0005.html "BitTorrent DHT spec") implementation.
+// DHT.debug = true;
 
-## install
+// for bootstrapping you need to know a node which already is in the dht
+dht.start();
+dht.bootstrap([ { 'address': 'xxx.xxx.xxx.xxx', 'port': xxx } ]);
 
-	npm install dht
+var id = DHT.util.hex2buf("640FE84C613C17F663551D218689A64E8AEBEABE");
 
-## usage
-
-	var DHT = require('dht');
-	var dht = new DHT.DHT(51414);
-
-	// DHT.debug = true;
-
-	// for bootstrapping you need to know a node which already is in the dht
-	dht.bootstrap([ { 'address': 'xxx.xxx.xxx.xxx', 'port': xxx } ]);
-
-	var id = DHT.util.hex2buf("640FE84C613C17F663551D218689A64E8AEBEABE");
-
-	dht.lookup(id, function (peers, finished) {
-		console.log("Found more peers: %j", peers);
-		if (finished) console.log("Lookup done");
-	});
-
-## status
-
-Local hashtable not implemented (needs to handle timeout of peers); announce not tested.
+dht.lookup(id, function (peers, finished) {
+	console.log("Found more peers: %j", peers);
+	if (finished) console.log("Lookup done");
+});
